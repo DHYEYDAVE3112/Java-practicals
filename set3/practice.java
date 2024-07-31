75a330e3a8faf916fdee9ec91aa3709e99e2167f
DHYEYDAVE3112 <152619294+DHYEYDAVE3112@users.noreply.github.com>
2024-07-31 14:15:09 +0530

    Create practice.java

---
+++ set3/practice.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+class practice
+{
+public static void main(String a[])
+{
+int x=Integer.parseInt(a[0]);
+int c=x*100;
+System.out.println("Value in rupees is "+c);
+}
+}
